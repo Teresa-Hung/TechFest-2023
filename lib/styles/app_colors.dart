25290f8fa331a8d113a4707e54b38eecae1b94f0
Teresa-Hung <teresac6123@gmail.com>
2023-01-16 15:24:00 +0800

    update homepage

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const pink = Color.fromRGBO(229, 115, 115, 1);
+  static const orange = Color.fromRGBO(255, 138, 101, 1);
+}
---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  //static const text1 = TextStyle(inherit: false, color: Colors.black);
+}
