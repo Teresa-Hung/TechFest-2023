493da667f37c1c6b2dd9bca9a7894bc61073b704
rheark <101083106+rheark@users.noreply.github.com>
2023-01-16 22:57:00 +0800

    code for log in

---
+++ petpark/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/rheakenneth/FlutterDev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rheakenneth/Downloads/techfest_project/techfest_project_2/petpark"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
