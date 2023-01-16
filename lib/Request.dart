c37901e834c906f8a2e0a5a4219ddd88150f1e59
bjiah <97540372+bjiah@users.noreply.github.com>
2023-01-16 22:41:56 +0800

    report stuff

---
+++ lib/Request.dart
@@ -0,0 +1,9 @@
+class Request{
+  String date;
+  String location;
+  String description;
+  String category;
+  String image;
+
+  Request({required this.date, required this.location, required this.category, required this.description, required this.image});
+}
